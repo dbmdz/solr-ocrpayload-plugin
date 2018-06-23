580530fb75b0d5ab2b72a57a23de96e69bb8ed83
Johannes Baiter <johannes.baiter@gmail.com>
2018-06-23 14:35:31 +0200

    Add test that uses a non-ascii delimiter

--- src/test/java/de/digitalcollections/solr/plugin/components/ocrhighlighting/AbsoluteHighlightingTest.java
+++ src/test/java/de/digitalcollections/solr/plugin/components/ocrhighlighting/AbsoluteHighlightingTest.java
@@ -12,9 +12,9 @@ public class AbsoluteHighlightingTest extends SolrTestCaseJ4 {
   @BeforeClass
   public static void beforeClass() throws Exception {
     initCore("conf/solrconfig.xml", "conf/schema.xml", "src/test/resources/solr", "min_absolute");
-    assertU(adoc("ocr_text", "two|x:12300,y:432,w:543,h:654, one|x:654,y:543,w:432,h:321,", "id", "101"));
-    assertU(adoc("ocr_text", "three|x:127,y:4820,w:5490,h:654, two|x:654,y:54337,w:431,h:341 five|x:0,y:0,w:0,h:0, "
-        + "four|x:111,y:111,w:111,h:111,", "id", "102"));
+    assertU(adoc("ocr_text", "two☛x:12300,y:432,w:543,h:654, one☛x:654,y:543,w:432,h:321,", "id", "101"));
+    assertU(adoc("ocr_text", "three☛x:127,y:4820,w:5490,h:654, two☛x:654,y:54337,w:431,h:341 five☛x:0,y:0,w:0,h:0, "
+        + "four☛x:111,y:111,w:111,h:111,", "id", "102"));
 
     assertU(commit());
   }
